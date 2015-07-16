9c7c66cf1a1166f012f697cbe4411878baa45c3e
Johann Egger <johann@codacy.com>
2015-07-16 12:02:24 +0300

    start one on port 9000 and one on 9001

--- app/Global.scala
+++ app/Global.scala
@@ -1,23 +1,30 @@
+import org.joda.time
+import org.joda.time.format.PeriodFormat
 import play.api.libs.ws.WS
 import play.api.{Play, Configuration, Application, GlobalSettings}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
 object Global extends GlobalSettings{
 
   override def onStart(app: Application) = {
-    Play.configuration(app).getBoolean("conf.server").collect{ case true =>
-      println("starting server")
-      t()(app)
-    }.getOrElse{ println("no server") }
+    implicit val appl = app
+    Play.configuration.getInt("http.port").collect{
+      case 9001 =>
+        println("server")
+        t(9000)
+      case 9000 =>
+        println("client")
+        u(9001)
+    }
     super[GlobalSettings].onStart(app)
   }
 
-  def t(times:Int=2,counter:Int=0)(implicit application: Application):Future[Long] = {
+  def t(port:Int, times:Int=128)(implicit application: Application):Future[Long] = {
     println(s"will perform $times requests")
     val reqs = (1 to times).map{ case index =>
-      WS.url("http://localhost:9000/indexThrottled").get().map{
+      WS.url(s"http://localhost:$port/indexThrottled").get().map{
         _.body.size
       }
     }
@@ -25,7 +32,19 @@ object Global extends GlobalSettings{
     Future.sequence(reqs).flatMap{ case done =>
       val sum = done.sum
       println(s"$times requests performed $sum")
-      t(times * 2).map{ case res => res +  sum}
+      t(port,times * 2).map{ case res => res +  sum }
+    }
+  }
+
+  def u(port:Int)(implicit application: Application): Unit = {
+    val start = time.DateTime.now()
+    WS.url(s"http://localhost:$port/index").get().onComplete{ case resp =>
+      val duration = new time.Duration(start, time.DateTime.now())
+      val s = PeriodFormat.getDefault().print(duration.toPeriod())
+      println(s"request took $s")
+      play.api.libs.concurrent.Promise.timeout("pause", 2.seconds).onComplete{ case pause =>
+        u(port)
+      }
     }
   }
 
